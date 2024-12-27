acb077f29b75c58af480a00fa7bef40ee6b3d8b9
JuYoung Kim <98570819+kjudud@users.noreply.github.com>
2024-12-28 00:43:51 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 208 ms, Memory: 42656 KB -BaekjoonHub

---
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
@@ -0,0 +1,13 @@
+import sys
+
+input = sys.stdin.readline
+result_set = set()
+for _ in range(int(input())):
+    tag = input()[:-1].split(' ')
+    if tag[1] == 'enter':
+        result_set.add(tag[0])
+    else:
+        result_set.remove(tag[0])
+
+for name in sorted(result_set, reverse=True):
+    print(name)
