7d446a4e72386207f37315ab74265ad7cfa89ade
JuYoung Kim <98570819+kjudud@users.noreply.github.com>
2024-12-30 01:02:56 +0900

    [Bronze I] Title: 유레카 이론, Time: 176 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10448. 유레카 이론/유레카 이론.py
+++ 백준/Bronze/10448. 유레카 이론/유레카 이론.py
@@ -6,14 +6,12 @@ T_n = [int(i*(i+1)/2) for i in range(1, 46)]
 
 def check(l):
     for i in range(45):
-        for j in range(45):
-            for k in range(45):
+        for j in range(i,45):
+            for k in range(j,45):
                 if (T_n[i] + T_n[j] + T_n[k]) == l:
                     return 1
     return 0
 
 for _ in range(int(input())):
-    result.append(check(int(input())))
+    print(check(int(input())))
 
-for i in result:
-    print(i)
